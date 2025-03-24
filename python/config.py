76464462c58ef4d55b6c63faf52865426c35b9ed
solclover <roadnet@gmail.com>
2025-03-24 19:47:54 +0800

    Initial commit

--- python/config.py
+++ python/config.py
@@ -5,4 +5,4 @@ import os
 MAIN_DIR = os.path.dirname(sys.path[0])
 
 # Create a list for position numbers
-POSITION = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14]
+POSITION = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16]
